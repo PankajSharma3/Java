dad65dad97227a90dbb5f893ec7b72ccf3f80a3c
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-26 20:52:25 +0530

    Create variables.java

---
+++ variables.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        String a = "Pankaj Sharma";
+        int b = 120;
+        boolean c = true;
+        char d = 'a';
+        float e = 3.4f;
+        System.out.println(a+'\n'+b+'\n'+c+'\n'+d+'\n'+e);
+    }
+}
