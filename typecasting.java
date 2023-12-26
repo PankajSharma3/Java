08325924f497b1999c32d1d22cdfae3c9bf69cc9
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-26 20:58:19 +0530

    Create typecasting.java

---
+++ typecasting.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        double a = 3.34;
+        int b = (int) a;
+        System.out.println(a);
+        System.out.println(b);
+    }
+}
