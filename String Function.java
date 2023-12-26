b3c373741ae195ded595d8d9d30fb4e8648f45dc
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-26 21:17:54 +0530

    Create String Function.java

---
+++ String Function.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        String txt = "Hello World";
+        System.out.println(txt.toUpperCase());
+        System.out.println(txt.toLowerCase()); 
+        System.out.println(txt.indexOf("W"));
+    }
+}
