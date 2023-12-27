e0801b03cb91559d12dfbd9c612fc7989502716f
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-27 20:34:36 +0530

    Create math.java

---
+++ math.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 132;
+        System.out.println(Math.max(a,b));
+        System.out.println(Math.min(a,b));
+        System.out.println(Math.sqrt(64));
+        System.out.println(Math.abs(-32.23));
+        int randomNum = (int)(Math.random() * 101);   //gives the random number between 1 and 100
+        System.out.println(randomNum);
+    }
+}
