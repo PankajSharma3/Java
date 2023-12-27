f0eee20ecfd29c7dbc32faf1b79c02b2970b2451
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-27 20:37:56 +0530

    Create If else ladder.java

---
+++ If else ladder.java
@@ -0,0 +1,12 @@
+class main{
+    public static void main(String[] args){
+        int a = 10;
+        int b = 22;
+        if(a>b)
+        System.out.println("a is greater than b");
+        else if(a==b)
+        System.out.println("a is equal to b");
+        else
+        System.out.println("a is smaller than b");
+    }
+}
