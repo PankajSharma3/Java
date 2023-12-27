f4b4db6841714d826fe7189dd2860ad404bb4c69
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-27 20:44:41 +0530

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,32 @@
+//While loop
+class main{
+    public static void main(String[] args){
+        int i=1;
+        while(i<11){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
+
+
+//Do while loop
+class main{
+    public static void main(String[] args){
+        int i=1;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<11);
+    }
+}
+
+
+//For loop
+class main{
+    public static void main(String[] args){
+        for(int i=1;i<11;i++){
+            System.out.println(i);
+        }
+    }
+}
