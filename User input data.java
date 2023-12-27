e91c5c61169a23c15d9d37389773bbd513ce7ac0
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2023-12-27 21:14:53 +0530

    Create User input data.java

---
+++ User input data.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+class main{
+    public static void main(String[] args){
+        Scanner a = new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        int b = a.nextInt();
+        System.out.println(b);
+    }
+}
