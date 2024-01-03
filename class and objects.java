1c32f0cf996edce71fb5e13ddefe10e14e59d722
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-03 10:58:37 +0530

    Create class and objects.java

---
+++ class and objects.java
@@ -0,0 +1,9 @@
+public class Main {
+  int x = 10;
+  public static void main(String[] args) {
+    Main myObj1 = new Main();  // Object 1
+    Main myObj2 = new Main();  // Object 2
+    System.out.println(myObj1.x);
+    System.out.println(myObj2.x);
+  }
+}
