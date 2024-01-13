a744aaed13d044edbfa1926f5f5b059033c7e868
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-13 12:39:39 +0530

    Create Default Constructor.java

---
+++ Default Constructor.java
@@ -0,0 +1,12 @@
+class Student{
+    int id;
+    String name; 
+    Student(){
+        System.out.println("Student is created");
+    }
+}
+class Main{
+    public static void main(String args[]){
+        Student s1 = new Student(); //constructor is called
+    }
+}
