9a9e31d3978e99a7f09b6a4aa290570e960e2029
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-14 11:08:47 +0530

    Create endsWith.java

---
+++ String/endsWith.java
@@ -0,0 +1,7 @@
+class Main{
+    public static void main(String[] args){
+        String a = "Fun with Java";
+        System.out.println(a.endsWith("ava"));
+        System.out.println(a.startsWith("Fun"));
+    }
+}
