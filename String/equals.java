6ed9eb68d036a5847819911b07374ab04b8f93e6
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-14 11:11:50 +0530

    Create equals.java

---
+++ String/equals.java
@@ -0,0 +1,9 @@
+class Main{
+    public static void main(String[] args){
+        String a = "Java";
+        String b = "Java";
+        String c = "Python";
+        System.out.println(a.equals(b));
+        System.out.println(a.equals(c));
+    }
+}
