bf38269c652511ef1966060680dbf321811a745c
Pankaj Sharma <118182606+PankajSharma3@users.noreply.github.com>
2024-01-14 11:06:17 +0530

    Create contains.java

---
+++ String/contains.java
@@ -0,0 +1,8 @@
+class Main{
+    public static void main(String[] args){
+        String a = "Fun with Java";
+        System.out.println(a.contains("Fun"));
+        System.out.println(a.contains("Java"));
+        System.out.println(a.contains("You"));
+    }
+}
